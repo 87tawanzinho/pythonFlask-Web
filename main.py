52fc199371be3a750b9d943c8abed039ccab9c89
tawan <tawan@gmail.com>
2024-02-11 07:48:50 -0800

    blue print learning

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
-from flask import Flask, url_for, render_template
+from flask import Flask
+from routes.home import home_route
 
 
 app = Flask(__name__) 
 
 
-
-
-
-#rotas
-@app.route('/', methods=['GET'])
-def hello_world():
-    titulo = "Gestão de Usuários"
-
-    usuarios = [
-        {"nome": "Guilherme", "membro_ativo": True},
-        {"nome": "Maria", "membro_ativo": False}
-    ]   
-    return render_template("index.html", titulo=titulo, usuarios=usuarios)
-
-
-@app.route('/sobre', methods=['GET'])
-def sayAboutMe():
-    return f"<a href='{ url_for('hello_world') }'>click aqui</a>"
-
-
-
-
+app.register_blueprint(home_route) # pega a rota 
 
 app.run(debug=True)
